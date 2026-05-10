42f1838f4295d9d87d6a87756d780527702c1062
David Puga <david.pugafudla.edu.ec>
2026-05-09 22:54:57 -0500

    Fix app.py

---
+++ gestion_licencias/migrations/0005_create_admin.py
@@ -0,0 +1,19 @@
+from django.db import migrations
+from django.contrib.auth.models import User
+
+def create_admin(apps, schema_editor):
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser(
+            username="admin",
+            password="admin123"
+        )
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('gestion_licencias', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(create_admin),
+    ]
