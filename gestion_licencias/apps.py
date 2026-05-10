72785c2a3a5b0e67d4829eabe68093893db76407
David Puga <david.pugafudla.edu.ec>
2026-05-09 22:49:34 -0500

    Creacion de admin temporal para el deploy

--- gestion_licencias/apps.py
+++ gestion_licencias/apps.py
@@ -1,5 +1,16 @@
 from django.apps import AppConfig
-
+from django.contrib.auth.models import User
 
 class GestionLicenciasConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'gestion_licencias'
+
+    def ready(self):
+        try:
+            if not User.objects.filter(username="admin").exists():
+                User.objects.create_superuser(
+                    username="admin",
+                    password="admin123"
+                )
+        except Exception:
+            pass
